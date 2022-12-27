9823fc0b64a6d27be77f8c5684c71f2ceb9748c3
tillmanfiber-dj <120396343+tillmanfiber-dj@users.noreply.github.com>
2022-12-27 12:11:43 -0500

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,5 +1,5 @@
 // paste in your published Google Sheets URL from the browser address bar
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1f077_M0ItoxGPUrdRu413WcZEwQ6a2i_8OLsQjSJ7HI/edit#gid=0';
-
+// var googleDocURL = 'https://docs.google.com/spreadsheets/d/1f077_M0ItoxGPUrdRu413WcZEwQ6a2i_8OLsQjSJ7HI/edit#gid=0';
+var googleDocURL    = 'https://docs.google.com/spreadsheets/d/1mXm1Lmt4cr2N77mb1WNgcuDxM-gOPtIgcY0YzWv4cu8/edit#gid=0';
 // insert your own Google Sheets API key from https://console.developers.google.com
 var googleApiKey = 'AIzaSyBh9nKnVZm2RPeZa0ywCOxPAgJJfK87WhY';
