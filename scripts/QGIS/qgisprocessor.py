236ff3e8d06d0db4d8ad27eaa26064ff6b3435ba
tillmanfiber-dj <120396343+tillmanfiber-dj@users.noreply.github.com>
2023-01-08 15:02:18 -0500

    Qgis Processing Data

--- scripts/QGIS/qgisprocessor.py
+++ scripts/QGIS/qgisprocessor.py
@@ -1,7 +1,36 @@
-import qgis.core
+import os
+import sys
+import json
+import pandas as pd
+
+# set up system paths
+qspath = './qgis_sys_paths.csv' 
+# provide the path where you saved this file.
+paths = pd.read_csv(qspath).paths.tolist()
+sys.path += paths
+
+# set up environment variables
+qepath = './qgis_env.json'
+js = json.loads(open(qepath, 'r').read())
+for k, v in js.items():
+    os.environ[k] = v
+
+# map the PROJ_LIB to handle the projections
+# for mac OS
+os.environ['PROJ_LIB'] = '/Applications/Qgis.app/Contents/Resources/proj'
+
+
+# qgis library imports
+import PyQt5.QtCore
+import gdal
+import qgis.PyQt.QtCore
+from qgis.core import (QgsApplication,
+                       QgsProcessingFeedback,
+                       QgsProcessingRegistry)
+from qgis.analysis import QgsNativeAlgorithms
 
 #set qgis path by checking QgisApplication.prefixPath()
-QgisApplication.prefixPath('/Application/QGIS.app/Contents/MacOS',True)
+QgsApplication.prefixPath('/Application/QGIS.app/Contents/MacOS',True)
 
 # Create a reference to the QgsApplication.  Setting the
 # second argument to False disables the GUI.
@@ -15,6 +44,8 @@ qgs.initQgis()
 print('Performing QGIS processing')
 # Write your code here to load some layers, use processing
 # algorithms, etc.
+layer = QgsVectorLayer(data_source, layer_name, provider_name)
+
 
 # Finally, exitQgis() is called to remove the
 # provider and layer registries from memory
